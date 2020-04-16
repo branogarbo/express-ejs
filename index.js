5d79cd82639f712bf81b9036785f6d566e89146c
Brian Longmore <brianl.ext@gmail.com>
2020-04-16 02:20:57 -1000

    added app.set to fix views path issue on server

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended: true}));
